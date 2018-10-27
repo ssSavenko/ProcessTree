6bd13c5e31d6c3b672d3752ccdaacc14876c548f
ss.savenko <ss.savenko@gmail.com>
2018-10-27 14:49:13 +0300

    Close #3

--- NativeLibrary/Source.cpp
+++ NativeLibrary/Source.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 #include <Windows.h>
 #include <TlHelp32.h>
+#include <vector>
 
 extern "C"
 {
-	static HANDLE snapshot;
+	static std::vector<PROCESSENTRY32> processes;
 
 	__declspec(dllexport) void MakeSnapshot()
 	{
-		snapshot = nullptr;
+		HANDLE snapshot = nullptr;
 		snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	}
-
-	__declspec(dllexport) int ParentProcessId(int processId)
-	{
-		int returnID = 0;
 
 		PROCESSENTRY32 entry = {};
 		entry.dwSize = sizeof(entry);
@@ -23,12 +19,23 @@ extern "C"
 		{
 			do
 			{
-				if (processId == entry.th32ProcessID)
-				{
-					returnID = entry.th32ParentProcessID;
-					break;
-				}
+				processes.push_back(entry);
 			} while (Process32Next(snapshot, &entry));
+		}
+		CloseHandle(snapshot);
+	}
+
+	__declspec(dllexport) int ParentProcessId(int processId)
+	{
+		int returnID = 0;
+
+		for (auto currentProcess = processes.begin(); currentProcess != processes.end; currentProcess++)
+		{
+			if (processId == currentProcess->th32ProcessID)
+			{
+				returnID = currentProcess->th32ParentProcessID;
+				break;
+			}
 		}
 
 		return returnID;
